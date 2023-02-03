11eccfcca1c6fcb1c92b48dc2b26dce8c8737c10
Shubham5250 <shubhampaw312@gmail.com>
2023-02-03 23:59:41 +0530

    home page-content added

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.fooddeliveryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+    }
+
+    public void mainActivity(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+        Animatoo.INSTANCE.animateSwipeLeft(this);
     }
 }
