eac71f5cffa622b58592f3ce889284648558d913
Shubham5250 <shubhampaw312@gmail.com>
2023-02-05 23:55:49 +0530

    BottomBar-icons-added-RecyclerView-Initiated

---
+++ app/src/main/java/com/example/fooddeliveryapp/Domain/CategoryDomain.java
@@ -0,0 +1,4 @@
+package com.example.fooddeliveryapp.Domain;
+
+public class CategoryDomain {
+}
--- app/src/main/java/com/example/fooddeliveryapp/MainActivity2.java
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity2.java
@@ -1,14 +1,30 @@
 package com.example.fooddeliveryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        
+        
+        recyclerViewCategoryList();
+    }
+
+    private void recyclerViewCategoryList() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
     }
 }
