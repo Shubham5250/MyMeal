e15f6a5cebcda8eea5ddb2a4c0d0f1215e3df3f4
Shubham5250 <shubhampaw312@gmail.com>
2023-02-11 23:59:40 +0530

    info-section-completed

--- app/src/main/java/com/example/fooddeliveryapp/info_activity.java
+++ app/src/main/java/com/example/fooddeliveryapp/info_activity.java
@@ -1,15 +1,124 @@
 package com.example.fooddeliveryapp;
 
+import static android.view.View.GONE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class info_activity extends AppCompatActivity {
+
+    TextView answer1, answer2, answer3, answer4;
+    ImageView arrQ1, arrQ2, arrQ3, arrQ4;
+    ImageView imageView5, imageView6, imageView7, imageView8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        answer3 = findViewById(R.id.answer3);
+        answer4 = findViewById(R.id.answer4);
+
+        arrQ1 = findViewById(R.id.arrQ1);
+        arrQ2 = findViewById(R.id.arrQ2);
+        arrQ3 = findViewById(R.id.arrQ3);
+        arrQ4 = findViewById(R.id.arrQ4);
+
+
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+
+
+        arrQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrQ1.setVisibility(GONE);
+                imageView5.setVisibility(View.VISIBLE);
+
+                answer1.setVisibility(View.VISIBLE);
+            }
+        });
+
+        arrQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrQ2.setVisibility(GONE);
+                imageView6.setVisibility(View.VISIBLE);
+                answer2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        arrQ3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrQ3.setVisibility(GONE);
+                imageView7.setVisibility(View.VISIBLE);
+
+                answer3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        arrQ4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrQ4.setVisibility(GONE);
+                imageView8.setVisibility(View.VISIBLE);
+
+                answer4.setVisibility(View.VISIBLE);
+            }
+        });
+
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView5.setVisibility(GONE);
+                answer1.setVisibility(GONE);
+                arrQ1.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView6.setVisibility(GONE);
+                answer2.setVisibility(GONE);
+                arrQ2.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView7.setVisibility(GONE);
+                answer3.setVisibility(GONE);
+                arrQ3.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView8.setVisibility(GONE);
+                answer4.setVisibility(GONE);
+                arrQ4.setVisibility(View.VISIBLE);
+            }
+        });
 
 
     }
