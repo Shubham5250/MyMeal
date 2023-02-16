a059d0a9d42024559fd24533db2e1774819279ad
Shubham5250 <shubhampaw312@gmail.com>
2023-02-16 23:55:01 +0530

    display-profile-activity-initiated

--- app/src/main/java/com/example/fooddeliveryapp/profile.java
+++ app/src/main/java/com/example/fooddeliveryapp/profile.java
@@ -9,16 +9,29 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class profile extends AppCompatActivity {
 
     ConstraintLayout logout;
+
+    private FirebaseUser user;
+    private DatabaseReference databaseReference;
+    private String userId;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profie);
 
         logout = findViewById(R.id.logout);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        userId = user.getUid();
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +40,8 @@ public class profile extends AppCompatActivity {
                 startActivity(new Intent(profile.this, register_user.class));
             }
         });
+
+
+
     }
 }
